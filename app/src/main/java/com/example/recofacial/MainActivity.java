ef683849758a23b00ef4d7521673f4ded35ae8d7
David <david_k2401@hotmail.com>
2019-06-25 08:25:12 -0500

    mongo connect

--- app/src/main/java/com/example/recofacial/MainActivity.java
+++ app/src/main/java/com/example/recofacial/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.mongodb.MongoClient;
+
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Open cv inicializado", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(getApplicationContext(),"Open cv error", Toast.LENGTH_SHORT).show();
+        MongoClient mongoClient = null;
+        try{
+            Toast.makeText(getApplicationContext(),"mongo conect", Toast.LENGTH_SHORT).show();
+            mongoClient= new MongoClient("localhost", 27017);
+        }catch(UnknownError error){
+            error.printStackTrace();
+        }
     }
 }
