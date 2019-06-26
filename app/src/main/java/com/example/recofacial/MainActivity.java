efd35da77d2a32b8a812ec3663ec968012955a45
David <david_k2401@hotmail.com>
2019-06-26 11:43:48 -0500

    start opencv

--- app/src/main/java/com/example/recofacial/MainActivity.java
+++ app/src/main/java/com/example/recofacial/MainActivity.java
@@ -3,23 +3,28 @@ package com.example.recofacial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.DocumentsContract;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-import java.net.UnknownHostException;
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-public class MainActivity extends AppCompatActivity {
-
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mat1, mat2, mat3;
+    BaseLoaderCallback baseLoaderCallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +35,66 @@ public class MainActivity extends AppCompatActivity {
         else
             Toast.makeText(getApplicationContext(),"Open cv error", Toast.LENGTH_SHORT).show();
 
-        try {
-            MongoClientURI uri = new MongoClientURI( "mongodb://localhost/db-name" );
-            MongoClient mongoClient = new MongoClient(uri);
-            MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
-        } catch (UnknownError e) {
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(),"Open cv error", Toast.LENGTH_SHORT).show();
-        }
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.myCamera);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
 
 
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat1 = inputFrame.rgba();
+        return mat1;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat3.release();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug())
+            Toast.makeText(getApplicationContext(),"problema al abrir openCv", Toast.LENGTH_SHORT).show();
+        else
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
     }
 }
