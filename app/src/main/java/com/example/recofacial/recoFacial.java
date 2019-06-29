af85e56c0cb0e28300b764ae420fcb5169a1a486
David <david_k2401@hotmail.com>
2019-06-29 00:43:09 -0500

    preview

--- app/src/main/java/com/example/recofacial/recoFacial.java
+++ app/src/main/java/com/example/recofacial/recoFacial.java
@@ -6,36 +6,93 @@ import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.OrientationEventListener;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 
-public class recoFacial extends AppCompatActivity implements SurfaceHolder.Callback {
+public class recoFacial extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private Camera camera;
-    private int Orientation;
-    private int OrientationCompensation;
-    private OrientationEventListener orientationEventListener;
-    private int DisplayRotation;
-    private int DisplayOrientation;
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mat1, mat2, mat3;
+    BaseLoaderCallback baseLoaderCallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reco_facial);
 
+
+        if (OpenCVLoader.initDebug())
+            Toast.makeText(getApplicationContext(),"Open cv inicializado", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Open cv error", Toast.LENGTH_SHORT).show();
+
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.myCamera);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
     }
 
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat1 = inputFrame.rgba();
+        return mat1;
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat3.release();
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug())
+            Toast.makeText(getApplicationContext(),"problema al abrir openCv", Toast.LENGTH_SHORT).show();
+        else
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
     }
 }
